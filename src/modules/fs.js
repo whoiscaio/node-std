c701e60a1a870c1e17f235e150d7b7d718e2d0d3
Caio <whoiscaio@protonmail.com>
2022-02-16 16:48:49 -0300

    feat: append file

--- src/modules/fs.js
+++ src/modules/fs.js
@@ -2,13 +2,29 @@ const fs = require('fs');
 const path = require('path');
 
 fs.mkdir(path.join(__dirname, '/teste'), (err) => {
-  if (err) return console.log(err);
+  if (err) {
+    return console.log(err);
+  }
 
   console.log('dir created');
+})
 
-  fs.writeFile(path.join(__dirname, '/teste', 'index.js'), "console.log('hello node fs')", (err) => {
+fs.writeFile(
+  path.join(__dirname, '/teste', 'text.txt'),
+  "my text",
+  (err) => {
     if(err) return console.log(err);
 
-    console.log('file created');
-  })
-})
+    console.log('file written');
+  }
+)
+
+fs.appendFile(
+  path.join(__dirname, '/teste', 'text.txt'),
+  "\nother line",
+  (err) => {
+    if(err) return console.log(err);
+
+    console.log('text appended');
+  }
+)
