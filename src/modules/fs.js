fe64422950cdd346ab8d7da6abd529f27380b78c
Caio <whoiscaio@protonmail.com>
2022-02-16 16:39:23 -0300

    feat: fs mkdir and write file on callback

--- src/modules/fs.js
+++ src/modules/fs.js
@@ -2,7 +2,13 @@ const fs = require('fs');
 const path = require('path');
 
 fs.mkdir(path.join(__dirname, '/teste'), (err) => {
-  if(err) return console.log(err);
+  if (err) return console.log(err);
 
-  console.log('Dir created');
-});
+  console.log('dir created');
+
+  fs.writeFile(path.join(__dirname, '/teste', 'index.js'), "console.log('hello node fs')", (err) => {
+    if(err) return console.log(err);
+
+    console.log('file created');
+  })
+})
