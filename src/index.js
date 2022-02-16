a8562be0c92c6119d203eaf6b5af8cf6b24536b0
Caio <whoiscaio@protonmail.com>
2022-02-16 17:02:40 -0300

    feat: first routes created

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-require('./modules/fs');
+const http = require('http');
+const users = require('./mocks/users');
+
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+  if(req.url === '/home') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('<h1>Hello World</h1>');
+  }
+
+  if(req.url === '/users') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(users));
+  }
+});
+
+server.listen(port, () => {
+  console.log('Running at: https://localhost:8080');
+})
