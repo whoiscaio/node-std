d230cb6064f43a549708c3abe4793dbeedbac9cb
Caio <whoiscaio@protonmail.com>
2022-02-16 16:54:34 -0300

    feat: read file and callback usage

--- src/modules/fs.js
+++ src/modules/fs.js
@@ -6,25 +6,33 @@ fs.mkdir(path.join(__dirname, '/teste'), (err) => {
     return console.log(err);
   }
 
-  console.log('dir created');
+  fs.writeFile(
+    path.join(__dirname, '/teste', 'text.txt'),
+    "my text",
+    (err) => {
+      if(err) return console.log(err);
+
+      fs.appendFile(
+        path.join(__dirname, '/teste', 'text.txt'),
+        "\nother line",
+        (err) => {
+          if(err) return console.log(err);
+
+          fs.readFile(
+            path.join(__dirname, '/teste', 'text.txt'),
+            "utf8",
+            (err, data) => {
+              if(err) return console.log(err);
+          
+              console.log(`
+              
+              ${data}
+          
+              `);
+            }
+          )
+        }
+      )
+    }
+  )
 })
-
-fs.writeFile(
-  path.join(__dirname, '/teste', 'text.txt'),
-  "my text",
-  (err) => {
-    if(err) return console.log(err);
-
-    console.log('file written');
-  }
-)
-
-fs.appendFile(
-  path.join(__dirname, '/teste', 'text.txt'),
-  "\nother line",
-  (err) => {
-    if(err) return console.log(err);
-
-    console.log('text appended');
-  }
-)
