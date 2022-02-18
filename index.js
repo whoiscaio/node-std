6e92b81dc36c5ad458175448afb97ff1f9c69fcd
Caio <whoiscaio@protonmail.com>
2022-02-18 15:39:47 -0300

    feat: env var set

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+const express = require('express');
+const router = require('./src/routes');
+const connect = require('./src/database/connect');
+
+const PORT = 3000;
+
+connect();
+
+const app = express();
+app.use(router);
+
+app.listen(PORT, () => {
+  console.log(`App running at https://localhost:${PORT}`);
+})
